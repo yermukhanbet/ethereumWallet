6d4a14cd5ce98b1989c2d9b9ba4fdf428f040450
Yessen Y <y.yesen.y@gmail.com>
2021-08-10 15:57:44 +0900

    cleaned the code. Moved away constant urls to global variables

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ from web3 import Web3
 import requests
 import json
 from datetime import datetime
-w3 = Web3(Web3.HTTPProvider('https://mainnet.infura.io/v3/fd71293b54954e358ed95da002a73549'))
+from decimal import Decimal
+
+mainnetURL = 'https://mainnet.infura.io/v3/fd71293b54954e358ed95da002a73549'
+def transactionUrl(publicAddress):
+    return 'https://api.etherscan.io/api?module=account&action=txlist&address={publicAddress}&startblock=0&endblock=99999999&sort=asc&apikey=WT8AHJ71QK5V3A3A47S8JWBWM38QN95FGN'
+
+w3 = Web3(Web3.HTTPProvider(mainnetURL))
 
 publicAddress = input("Input your address: ")
 balance = w3.eth.get_balance(publicAddress)
@@ -12,24 +18,17 @@ print("your balance is: ", balance)
 transactionCount = w3.eth.getTransactionCount(publicAddress)
 print("transaction count: ", transactionCount)
 
-r = requests.get(
-    f'https://api.etherscan.io/api?module=account&action=txlist&address={publicAddress}&startblock=0&endblock=99999999&sort=asc&apikey=WT8AHJ71QK5V3A3A47S8JWBWM38QN95FGN')
+r = requests.get(transactionUrl(publicAddress))
 data = r.json()
-print(r.url)
-for key, value in data.items():
-    print(key, ":", value)
-
-datas = json.loads(r.text)
-print(datas)
-
 
 def gweiToEth(gwei):
-    return float(gwei) / 1000000000000000000
+    eth = w3.fromWei(gwei, 'ether')
+    return eth
 def timeCreated(time):
     date = datetime.fromtimestamp(time)
     return date
 
-for transaction in datas['result']:
+for transaction in data['result']:
     print('from: ', transaction['from'])
     print('to: ', transaction['to'])
     gweiAmount = gweiToEth(float(transaction['value']))
